bd9ca3e1f1c90909d747bedf3e84b26773cde62f
RookieAND <gwangin1999@naver.com>
2025-01-12 23:21:19 +0900

    feat: OpenGraph 적용 및 기초 Color Theme Token 적용

--- apps/admin/tailwind.config.cjs
+++ apps/admin/tailwind.config.cjs
@@ -4,6 +4,28 @@ module.exports = {
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
     ],
-    theme: {},
+    theme: {
+        colors: {
+            main: {
+                primary: '#0C3FC1',
+                secondary: '#ECF0FF',
+                tertiary: '#D7DCEB',
+            },
+            text: {
+                primary: '#181818',
+                secondary: '#6F6F80',
+                tertiary: '#9E9EA3',
+                quaternary: '#AAB2D0',
+            },
+            border: {
+                primary: '#ECEEF5',
+                secondary: '#F2F4F9',
+            },
+            background: {
+                white: '#FBFCFE',
+                gradient: 'linear-gradient(0deg, #194FDC 100%, #6E5ED9 100%)',
+            }
+        }
+    },
     plugins: []
 }
--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,10 +1,27 @@
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
+import ogPlugin, { type Options as OpenGraphConfig } from 'vite-plugin-open-graph';
+
+const openGraphConfig: OpenGraphConfig = {
+    basic: {
+        title: '데브말싸미 어드민 | 개발 용어 발음 사전',
+        type: 'website',
+        image: {
+            url: 'https://dev-malssami-bucket.s3.ap-northeast-2.amazonaws.com/assets/opengraph-image.png',
+        },
+        url: 'https://api.dev-malssami.site',
+        description:
+            '데브말싸미 사이트를 운영하고 관리하기 위해 신설된 어드민 페이지입니다.',
+        determiner: 'the',
+        locale: 'ko_KR',
+        siteName: '데브말싸미 어드민',
+    },
+}
 
 // https://vite.dev/config/
 export default defineConfig({
     resolve: {
         alias: [{ find: "#/", replacement: "/src/" }]
     },
-    plugins: [react()]
+    plugins: [react(), ogPlugin(openGraphConfig)],
 })
